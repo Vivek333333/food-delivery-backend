6e7538fe3df08d7d0066f377fd267c1b9593944c
Vivek333333 <vivekpatel09638@gmail.com>
2026-08-06 17:59:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app.add_middleware(
 app.mount("/images", StaticFiles(directory="images"), name="images")
 
 # Dynamic IP string used for formatting your media assets securely
-BASE_MEDIA_URL = "https://frontend.3utilities.com/images/"
+BASE_MEDIA_URL = "https://johnmarcos.online/images/"
 
 # Global in-memory storage for OTPs (Key: phone number, Value: dict with OTP and expiry time)
 otp_store: Dict[str, dict] = {}
